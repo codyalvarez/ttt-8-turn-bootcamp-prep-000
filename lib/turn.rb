c129ee71bfd82b363f93f6d4ee2daa3de3a2709f
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-21 23:33:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,6 +17,14 @@ def valid_move?(board, index)
   end
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
 
 
 def move
