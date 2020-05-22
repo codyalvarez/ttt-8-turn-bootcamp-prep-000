4abf5738b0af92c7aafeb5a7950f6a11fc151876
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-22 01:51:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,7 +29,7 @@ def turn(user_input)
   input_to_index(user_input)
   if valid_move?(index) == true && current_player == "X"
     move(index, value = "X")
-  #   puts display_board
+    puts display_board
   # elsif valid_move?(index) == true && current_player == "O"
   #   move(index, value = "O")
   #   puts display_board
