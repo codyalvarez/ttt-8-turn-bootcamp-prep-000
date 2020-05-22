7d57f88e4d902093821f4e0f26912ed72d8c2c80
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-22 02:10:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,21 +40,21 @@ def current_player
   turn_count.even? == true ? "X" : "O"
 end
 
-def turn(user_input)
+def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.chomp
-  index = input_to_index(user_input)
-  if valid_move?(index) == true && current_player == "X"
-    move(index, value = "X")
-    puts display_board
-  elsif valid_move?(index) == true && current_player == "O"
-    move(index, value = "O")
-    puts display_board
-  elsif valid_move?(index) == false
-    puts "Please enter 1-9:"
-    input = gets.chomp
-  else
-    puts "Please enter 1-9:"
-    input = gets.chomp
-  end
+  # user_input = gets.chomp
+  # index = input_to_index(user_input)
+  # if valid_move?(index) == true && current_player == "X"
+  #   move(index, value = "X")
+  #   puts display_board
+  # elsif valid_move?(index) == true && current_player == "O"
+  #   move(index, value = "O")
+  #   puts display_board
+  # elsif valid_move?(index) == false
+  #   puts "Please enter 1-9:"
+  #   input = gets.chomp
+  # else
+  #   puts "Please enter 1-9:"
+  #   input = gets.chomp
+  # end
 end
