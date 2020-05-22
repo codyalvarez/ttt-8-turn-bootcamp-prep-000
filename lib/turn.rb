80c70acb7d72b47edde1ebe90bb3fa89723c3236
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-22 00:10:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,6 +12,10 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
+  if board[index] == "X" || board[index] == "O" || !index.betwween?(0, 8)
+    return false 
+  else
+    return true
 end
 
 def move
