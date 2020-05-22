f0b4c62ce0ad47e451b155d99244d5caa31de8c4
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-22 02:07:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,7 +43,7 @@ end
 def turn(user_input)
   puts "Please enter 1-9:"
   user_input = gets.chomp
-  input_to_index(user_input)
+  index = sinput_to_index(user_input)
   if valid_move?(index) == true && current_player == "X"
     move(index, value = "X")
     puts display_board
